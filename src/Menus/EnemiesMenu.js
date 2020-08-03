760c5150a672a6f5195e5f542bb79db07191a4bf
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-08-03 19:58:57 +0530

    Add menus for UIScene

---
+++ src/Menus/EnemiesMenu.js
@@ -0,0 +1,12 @@
+import 'phaser';
+import Menu from './Menu';
+
+export default class EnemiesMenu extends Menu {
+  constructor(x, y, scene) {
+    super(x, y, scene);                    
+  }
+
+  confirm() {
+    
+  }
+}
